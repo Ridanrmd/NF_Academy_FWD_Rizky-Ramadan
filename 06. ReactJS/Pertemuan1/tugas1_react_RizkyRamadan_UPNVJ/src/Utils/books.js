88cb44c2b854a031928249853b1c6eab0d075771
Rizky Ramadan <ramadanrizky593@gmail.com>
2025-10-01 20:26:08 +0700

    upload tugas

---
+++ 06. ReactJS/Pertemuan1/tugas1_react_RizkyRamadan_UPNVJ/src/Utils/books.js
@@ -0,0 +1,20 @@
+const books = [
+  {
+    id: 1,
+    title: "Belajar JavaScript Dasar",
+    author: "Andi Prasetyo",
+    year: 2021,
+    description:
+      "Panduan lengkap untuk pemula yang ingin belajar JavaScript dari nol.",
+    image: "https://example.com/images/js-dasar.jpg",
+  },
+  {
+    id: 2,
+    title: "React untuk Pemula",
+    author: "Dina Sari",
+    year: 2022,
+    description: "Mengenai konsep dan praktik membuat aplikasi React modern.",
+    image: "https://example.com/images/react-pemula.jpg",
+  },
+];
+export default books;
